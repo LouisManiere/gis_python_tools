d26a54bedf6e5cc611910fd564ba900a8158a29f
Louis Manière <louismaniere@orange.fr>
2023-05-04 11:32:03 +0200

    standardisation with config

--- copy_raster_tiles_from_tileset.py
+++ copy_raster_tiles_from_tileset.py
@@ -1,4 +1,4 @@
-
+import os
 from raster_tools.raster_tools import extract_raster_tiles_from_tileset
 from config.config import paths_config, parameters_config
 
@@ -8,7 +8,7 @@ params = parameters_config()
 
 
 extract_raster_tiles_from_tileset(
-    tileset_path = './inputs/tileset.gpkg',
+    tileset_path = os.path.join(paths['inputs_dir'], paths['tileset']),
     raster_dir = paths['inputs_dir'],
     dest_dir = paths['outputs_dir']
 )
--- create_tileset_from_tiled_raster.py
+++ create_tileset_from_tiled_raster.py
@@ -1,4 +1,4 @@
-
+import os
 from raster_tools.raster_tools import extract_raster_extent
 from config.config import paths_config, parameters_config
 
@@ -10,6 +10,6 @@ params = parameters_config()
 extract_raster_extent(
     input_dir_path = paths['inputs_dir'],
     extension = params['raster_extension'],
-    tileset_path = './outputs/tileset.gpkg',
+    tileset_path = os.path.join(paths['outputs_dir'], paths['tileset']),
     crs = params['crs']
 )
