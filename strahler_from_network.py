a4b0631f298341bc00354aa71497bfbaf60ae7e2
Manière Louis <louismaniere@orange.fr>
2023-09-01 15:45:27 +0200

    add strahler from network tool

---
+++ strahler_from_network.py
@@ -0,0 +1,5 @@
+from vector_tools.vector_tools import StrahlerOrder
+
+StrahlerOrder('C:/Users/lmanie01/Documents/Gitlab/mapdotoro/data-raw/network.gpkg', 
+              'C:/Users/lmanie01/Documents/Gitlab/mapdotoro/data-raw/network_strahler.gpkg', 
+              overwrite=True)
--- vector_tools/vector_tools.py
+++ vector_tools/vector_tools.py
@@ -13,6 +13,8 @@ DOCME
 ***************************************************************************
 """
 
+import os
+import click
 import fiona
 import fiona.crs
 from shapely.geometry import shape, box
@@ -334,3 +336,170 @@ def JoinNetworkAttributes(params, tileset = 'default'):
                                         'geometry': line['geometry'],
                                     }
                         output.write(rhts_feature)
+
+def StrahlerOrder(hydro_network, hydrography_strahler, overwrite=True):
+    """
+    Calculate Strahler stream order
+    Parameters:
+    - params (object): An object containing the parameters.
+        - hydro_network (str): The filename of the hydro network.
+        - hydrography_strahler (str): The filename for hydro network with Strahler order.
+    - overwrite (bool): Optional. Specifies whether to overwrite existing tiled buffer files. Default is True.
+    - source code from https://here.isnew.info/strahler-stream-order-in-python.html
+
+    Returns:
+    - None
+
+    """
+    click.secho('Compute Strahler order', fg='yellow')
+
+    # check overwrite
+    if os.path.exists(hydrography_strahler) and not overwrite:
+        click.secho('Output already exists: %s' % hydrography_strahler, fg='yellow')
+        return
+
+    # function to find head line in network (top upstream)
+    def find_head_lines(lines):
+        head_idx = []
+
+        num_lines = len(lines)
+        for i in range(num_lines):
+            line = lines[i]
+            first_point = line[0]
+
+            has_upstream = False
+
+            for j in range(num_lines):
+                if j == i:
+                    continue
+                line = lines[j]
+                last_point = line[len(line)-1]
+
+                if first_point == last_point:
+                    has_upstream = True
+
+            if not has_upstream:
+                head_idx.append(i)
+
+        return head_idx
+
+    # function to find next line downstream
+    def find_next_line(curr_idx, lines):
+        num_lines = len(lines)
+
+        line = lines[curr_idx]
+        last_point = line[len(line)-1]
+
+        next_idx = None
+
+        for i in range(num_lines):
+            if i == curr_idx:
+                continue
+            line = lines[i]
+            first_point = line[0]
+
+            if last_point == first_point:
+                next_idx = i
+                break
+
+        return next_idx
+
+    # function to find sibling line (confluence line)
+    def find_sibling_line(curr_idx, lines):
+        num_lines = len(lines)
+
+        line = lines[curr_idx]
+        last_point = line[len(line)-1]
+
+        sibling_idx = None
+
+        for i in range(num_lines):
+            if i == curr_idx:
+                continue
+            line = lines[i]
+            last_point2 = line[len(line)-1]
+
+            if last_point == last_point2:
+                sibling_idx = i
+                break
+
+        return sibling_idx
+
+    # read reference network
+    with fiona.open(hydro_network, 'r') as source:
+
+        schema = source.schema.copy()
+        driver=source.driver
+        crs=source.crs
+
+        # define new fields
+        strahler_field_name = "strahler"
+        strahler_field_type = 'int'
+        # Add the new field to the schema
+        schema['properties'][strahler_field_name] = strahler_field_type
+
+        lines = []
+        source_copy = []
+
+        # copy feature with strahler field in source_copy and the the line coordinates in lines
+        for feature in source:
+                # Create a new feature with the new field
+                new_properties = feature['properties']
+                new_properties[strahler_field_name] = 0  # Set the strahler field value to 0
+                geom = shape(feature['geometry'])
+                # copy line coordinates to find head line
+                line = geom.coords
+                lines.append(line)
+                # copy features in new list to update the data before write all
+                source_copy.append(feature)
+
+        # save head lines index
+        head_idx = find_head_lines(lines)
+
+        with click.progressbar(head_idx) as processing:
+            for idx in processing:
+                curr_idx = idx
+                curr_ord = 1
+                # head lines order = 1
+                source_copy[curr_idx]['properties'][strahler_field_name] = curr_ord
+                # go downstream from each head lines
+                while True:
+                    # find next line downstream
+                    next_idx = find_next_line(curr_idx, lines)
+                    # stop iteration if no next line
+                    if not next_idx:
+                        break
+                    # copy next line feature and order
+                    next_feat = source_copy[next_idx]
+                    next_ord = next_feat['properties'][strahler_field_name]
+                    # find sibling line
+                    sibl_idx = find_sibling_line(curr_idx, lines)
+                    # if sibling line exist
+                    if sibl_idx is not None:
+                        # copy sibling line feature and order
+                        sibl_feat = source_copy[sibl_idx]
+                        sibl_ord = sibl_feat['properties'][strahler_field_name]
+                        # determinate order base on sibling, next and current line
+                        if sibl_ord > curr_ord:
+                            break
+                        elif sibl_ord < curr_ord:
+                            if next_ord == curr_ord:
+                                break
+                        else:
+                            curr_ord += 1
+                    # update order in feature copy dict
+                    source_copy[next_idx]['properties'][strahler_field_name] = curr_ord
+                    # go further downstream
+                    curr_idx = next_idx
+
+                # write final features from updated features copy
+                with fiona.open(hydrography_strahler, 'w', driver=driver, crs=crs, schema=schema) as modif:
+                    for feature in source_copy:
+                        if feature['properties'][strahler_field_name] > 0:
+                            modified_feature = {
+                                    'type': 'Feature',
+                                    'properties': feature['properties'],
+                                    'geometry': feature['geometry'],
+                                }
+
+                            modif.write(modified_feature)
